1fecc4f2eb40d03a4c87dc258da599ebf65d5c49
SAIRUS MISHRA <52784770+iamsairus10@users.noreply.github.com>
2021-11-23 01:38:06 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () =>{
     return (
         <div>
-            <h6 className="text-center text-warning mt-3 mb-4">Made by Aitik with ❤️</h6>
+            <h6 className="text-center text-warning mt-3 mb-4">Made by Sairus with ❤️</h6>
         </div>
     )
 }
